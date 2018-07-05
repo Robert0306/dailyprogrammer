b9e09ee6307ad80fd8f1fac13631f6480bd66054
Robert0306 <Robertkaino-Jobb@Roberts-MacBook-Air.local>
2018-07-05 23:39:56 +0200

    update

---
+++ NestedLoops.java
@@ -0,0 +1,31 @@
+// staircase problem from hackerRank.
+
+public class NestedLoops {
+  public static void main(String[] args) {
+    quadPattern(4);
+    System.out.println("");
+    stairCase(4);
+
+
+  }
+
+  public static void quadPattern(int n){
+    // quadratic patterns
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+
+  public static void stairCase(int n){
+    // staircase with #
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j <= i; j++){
+        System.out.print("#");
+      }
+      System.out.println("");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Some basic algorithms and other small scripts/code.
      Give a number and return the sum of the given number. example; 123 should be 1 + 2 + 3 = 6 </li>
 <li> Codewars: FindCapitals, Binarylist, avragelist, give a int on expanded form. - small sorting algorithms, build Tower: use a simple list comparison to format list indexes to a pyramid<br>
 tvRemote: count and search in 2d-array. HighestProfit: find high and low value in array.<br>
+<li>Random algorithms and HackerRank</li><br>
+Different NestedLoops examples. 
